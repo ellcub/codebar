4a85accf6f6c1b034eebc83b5ffad3821fb382d6
ellcub <ellen@beebell.co.uk>
2015-06-11 16:12:15 +0100

    Set preview box background colour

--- javascript/colorpicker/script.js
+++ javascript/colorpicker/script.js
@@ -1,1 +1,14 @@
 /* Exercise 2: Color picker */
+$(document).ready(function() {
+
+  $(document).on('keydown keyup keypress', '#color', function() {
+    color = $(this).val();
+    setPreviewColor(color);
+  })
+
+})
+
+function setPreviewColor(color) {
+  // set background color
+  $('.preview').css('background-color', color);
+}
