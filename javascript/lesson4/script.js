1dd7b180a36c3d568833dc0f39d34ab4d95e21b4
ellcub <ellen@beebell.co.uk>
2015-06-25 15:21:10 +0100

    Show user

--- javascript/lesson4/script.js
+++ javascript/lesson4/script.js
@@ -1,13 +1,22 @@
 // github user finder example
 function getGithubInfo(username) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.github.com/users/" + username, true);
+  xhr.open("GET", "https://api.github.com/users/" + username, false);
   xhr.send();
   return xhr;
 }
 
 function noSuchUser(username) {
-  $("#profile h2").html("No such user: " + username)
+  $("#profile h2").html("No such user: " + username);
+}
+
+function showUser(user) {
+  // Display the user's Github id
+  $("#profile h2").html(user.login + " is GitHub user #" + user.id);
+  // Add a link to the user's Github profile in #profile .information. The link should have a class profile
+  $("#profile .information").html("<a href='" + user.html_url + "' class='profile'>" + user.login + "</a>");
+  // Add an image in #profile .avatar. To do that, you can use the avatar_url from the response.
+  $("#profile .avatar").html("<img src='" + user.avatar_url + "' alt='" + user.login + "'>");
 }
 
 
@@ -17,12 +26,10 @@ $(document).ready(function() {
     if (e.which === 13) {
       // get val() from input field
       var username = $('#username').val();
-      console.log(username);
       // assign getGithubInfo(username) to a variable response
       var response = getGithubInfo(username);
-      console.log(response);
       if (response.status === 200) {
-
+        showUser(JSON.parse(response.responseText));
       } else {
         noSuchUser(username);
       }
