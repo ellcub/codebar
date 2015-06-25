2c59f587e2dc8190d6d939c7a7e246ebd3975b11
ellcub <ellen@beebell.co.uk>
2015-06-25 12:22:42 +0100

    Get response from API

--- javascript/lesson4/script.js
+++ javascript/lesson4/script.js
@@ -1,18 +1,23 @@
 // github user finder example
 function getGithubInfo(username) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.github.com/", true);
-
+  xhr.open("GET", "https://api.github.com/users/" + username, true);
+  xhr.send();
   return xhr;
 }
 
 
 $(document).ready(function() {
   $(document).on('keypress', '#username', function(e) {
+
     if (e.which === 13) {
       // get val() from input field
-
+      var username = $('#username').val();
+      console.log(username);
       // assign getGithubInfo(username) to a variable response
+      var response = getGithubInfo(username);
+      console.log(response);
     }
+
   })
 });
